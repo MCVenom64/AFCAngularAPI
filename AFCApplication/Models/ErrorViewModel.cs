4335c29f60b2aff2935a3f12a8dd5caaa07d8b26
garth <garth@MEGATRON>
2024-03-29 14:22:35 +0000

    Add project files.

---
+++ AFCApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AFCApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
