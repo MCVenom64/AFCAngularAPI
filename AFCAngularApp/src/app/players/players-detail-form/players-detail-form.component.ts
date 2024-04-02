b43fca3d30d9cd2a47ede28135a00b1488c47fb6
Garth Simoyi <garthsimoyi@hotmail.com>
2024-04-02 20:15:04 +0100

    playerform

--- AFCAngularApp/src/app/players/players-detail-form/players-detail-form.component.ts
+++ AFCAngularApp/src/app/players/players-detail-form/players-detail-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PlayersComponent } from "../players.component";
 import { PlayersService } from '../../shared/players.service';
-import {  FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
 import { Players } from '../../shared/players.model';
@@ -11,51 +11,51 @@ import { ToastrService } from 'ngx-toastr';
     standalone: true,
     templateUrl: './players-detail-form.component.html',
     styles: ``,
-    imports: [PlayersComponent,CommonModule,FormsModule]
+    imports: [PlayersComponent, CommonModule, FormsModule]
 })
 export class PlayersDetailFormComponent {
-   
+    //List of players - to extend thisthi will be an aditional call to the API to get a list of players from database.
+    positions = ['GoalKeeper', 'Defender', 'Midfielder', 'Striker'];
 
-    constructor(public service : PlayersService,private toastr:ToastrService){
-
+    constructor(public service: PlayersService, private toastr: ToastrService) {
     }
 
-    onSubmit(form:NgForm){
+    onSubmit(form: NgForm) {
         this.service.formSubmitted = true
-        if(form.valid){
-if(this.service.formData.playerId == 0){
- this.addPlayer(form)
-}
-else{
-    this.updatePlayer(form)
-}
-      
-}
-    
+        if (form.valid) {
+            if (this.service.formData.playerId == 0) {
+                this.addPlayer(form)
+            }
+            else {
+                this.updatePlayer(form)
+            }
+
+        }
+
     }
-    addPlayer(form:NgForm){
+    addPlayer(form: NgForm) {
         this.service.playerSubmit()
-        .subscribe({
-            next: res=>{
-            this.service.list = res as Players[]
-            this.service.resetForm(form);
-            this.toastr.success('Saved sucessfully', 'Player Detail')
-           },
-            error:err => {console.log(err)}
-        })
+            .subscribe({
+                next: res => {
+                    this.service.list = res as Players[]
+                    this.service.resetForm(form);
+                    this.toastr.success('Saved sucessfully', 'Player Detail')
+                },
+                error: err => { console.log(err) }
+            })
     }
-    updatePlayer(form:NgForm){
+    updatePlayer(form: NgForm) {
 
-       this.service.playerUpdate()
-       .subscribe({
-        next: res=>{
-        this.service.list = res as Players[]
-        this.service.resetForm(form);
-        this.toastr.info('Updated sucessfully', 'Player Detail')
-       },
-        error:err => {console.log(err)}
-    })
+        this.service.playerUpdate()
+            .subscribe({
+                next: res => {
+                    this.service.list = res as Players[]
+                    this.service.resetForm(form);
+                    this.toastr.info('Updated sucessfully', 'Player Detail')
+                },
+                error: err => { console.log(err) }
+            })
 
-       
+
     }
 }
