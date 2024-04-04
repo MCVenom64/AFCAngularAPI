68ad6201ac62a2fe0727c131b1914b337899403b
Garth Simoyi <garthsimoyi@hotmail.com>
2024-04-04 00:24:09 +0000

    Deleted AFCApplication

--- AFCApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AFCApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
